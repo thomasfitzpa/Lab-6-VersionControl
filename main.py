5e01d9c312e20ba2d4a632866523062bc36ea44a
thomasfitzpa <thomas.fitzpatri@ufl.edu>
2023-10-30 13:06:50 -0400

    new code

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 def encode(password):
-    x = 0
-    while x < len(password):
-        password = password
 
-    return password
-w
+    x = 0
+    encoded_password = ""
+    for char in password:
+        char_value = int(char)
+
+        if char_value < 6:
+            encoded_password += str(char_value + 4)
+        else:
+            value = str(char_value + 4)
+            char_value = value[1]
+            encoded_password += char_value
+
+    return encoded_password
+
+
+print(encode("123456789"))
